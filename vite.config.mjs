d1d5c636588e56e7a36036aa3d545f674ff0ac2b
ryoryoryo3838 <miya.klis.default@gmail.com>
2025-06-13 14:04:10 +0900

    actions

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 export default defineConfig({
+  root: ".",
   plugins: [nodeResolve()],
   server: {
     watch: {
