08dc6612d848a4472854d77f178d255707d7ab3d
ryoryoryo3838 <miya.klis.default@gmail.com>
2025-09-15 22:54:58 +0000

    fix bug

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import tailwindcss from '@tailwindcss/vite'
-import string from "vite-plugin-string"
 
 export default defineConfig({
   root: ".",
   plugins: [
     nodeResolve(),
     tailwindcss(),
-    string({
-      include: ["**/*.html"],
-    }),
   ],
   server: {
     watch: {
