46bca5207f9088bd059e315db9a4270d7cc368f7
ryoryoryo3838 <miya.klis.default@gmail.com>
2025-09-17 01:36:51 +0000

    fix: d1

--- pages/functions/api/db.ts
+++ pages/functions/api/db.ts
@@ -5,22 +5,22 @@ interface Env {
 
 export const onRequestPost: PagesFunction<Env> = async ({ request, env }) => {
   try {
-    const sql  = await request.text();
+    const { txt } = await request.json();
     
     // プレースホルダー `?` と `.bind()` を使用
-    const result = await env.DB.prepare(sql).run();
+    const result = await env.DB.prepare("INSERT INTO CONTENTS (Content) VALUES (?)").bind(txt).run();
 
     if (result.meta.changes > 0 ) {
       return new Response(JSON.stringify(
         { message: "SQLs送信成功",
-          executeSql: sql
+          executeSql: txt
         }
       ), { status: 201 });
     }
     
     return new Response(JSON.stringify(
       { message: "SQL送信失敗",
-        executeSql: sql
+        executeSql: txt
       }), { status: 500 });
   } catch (err) {
     return new Response(JSON.stringify({ error: err.message }), { status: 500 });
--- src/components/ReactJS/DB.jsx
+++ src/components/ReactJS/DB.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 function UserForm() {
-  const [sql, setSql] = useState('');
+  const [text, setText] = useState('');
   const [response, setResponse] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -13,15 +13,15 @@ function UserForm() {
       const res = await fetch('/api/db', {
         method: 'POST',
         headers: {
-          'Content-Type': 'text/plain',
+          'Content-Type': 'application/json',
         },
-        body: sql,
+        body: JSON.stringify({txt: text}),
       });
 
       const data = await res.json();
       console.log(data.message);
       console.log(data.executeSql);
-      setResponse(data.message);
+      setResponse(data);
 
     } catch (error) {
       console.error("API呼び出しエラー:", error);
@@ -36,12 +36,12 @@ function UserForm() {
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          value={sql}
-          onChange={(e) => setSql(e.target.value)}
-          placeholder="ユーザー名を入力"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="追加したいテキストを入力"
         />
         <button type="submit" disabled={loading}>
-          {loading ? '送信中...' : 'ユーザーを作成'}
+          {loading ? '送信中...' : 'テキストを追加'}
         </button>
       </form>
       {response && (
